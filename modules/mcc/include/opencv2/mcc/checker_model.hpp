38efea529b62828279c0f105d63ebf268f941507
AYELET0527661122 <A0527661122@GMAIL.COM>
2026-03-09 14:15:37 +0200

    Fix formatting in checker_model.hpp

--- modules/mcc/include/opencv2/mcc/checker_model.hpp
+++ modules/mcc/include/opencv2/mcc/checker_model.hpp
@@ -50,7 +50,6 @@ enum TYPECHART
     VINYL18,   ///< DKK color chart with 12 squares and 6 rectangle
     SPYDERCHECKR48_V2, // 6x8 = 48 squares
     SPYDERCHECKR24_V2  // 6x4 = 24 squares
-
 };
 
 /** CChecker
